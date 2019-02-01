36903d34389c63338a35169b45669c7ecbf68d4e
Aaron Strouse <aaronstrouse@gmail.com>
2019-02-01 16:17:36 -0500

    added api endoints for quotes, random, index

--- Node/express-server.js
+++ Node/express-server.js
@@ -2,20 +2,21 @@
 // DATABASE SETUP ****************************
 // *******************************************
 var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017/test' // test db on localhost
-var quotes  // mongo collection
-var topquote   // tracks the largest / highest index in db
+var url = 'mongodb://localhost:27017/test'
+var quotes
+var topquote
 
 // *******************************************
 // EXPRESS SETUP  ****************************
 // *******************************************
 
-var express = require('express'); // control all http interaction by server
+var express = require('express');
 var app = express();
 var router = express.Router();    
 var path = require('path');
 var bodyParser = require('body-parser');
 
+app.use('/demo', express.static(path.join(__dirname, '..', '..', '..','static')));
 app.use(bodyParser.json());
 app.set('json spaces', 2);
 
@@ -24,9 +25,41 @@ app.set('json spaces', 2);
 // *******************************************
 
 // index with helpful message
-app.get('/', function(request, reply) {  // 2nd argument function is the callback
+app.get('/', function(request, reply) {
   reply.send('Hello world from express');
 });
+
+// *******************************************
+// REST API ROUTES ***************************
+// *******************************************
+
+app.use('/api', router); // all routes will be found at /api/quotes
+
+// QUOTE LIST
+router.route('/quotes')  // app processes request based on combo of endoint & method(s) -> dot.notation
+  .get(function(request, reply)  {
+      quotes.find().sort({index:-1}).limit(10).toArray((err, results) => {
+	      reply.send(results)
+      })
+  })
+
+// RANDOM QUOTE FROM THE DATABASE
+router.route('/quotes/random')
+  .get(function(request, reply) {
+    var random = Math.floor(Math.random()*topquote)
+    quotes.findOne({"index":random}, (err, results) => {
+       reply.send(results)
+    })
+  })
+
+// SINGLE QUOTE
+router.route('/quotes/:index')
+  .get(function(request, reply) {
+    index = parseInt(request.params.index) // pulls index from the path / parseInt takes from string to int
+    quotes.findOne({"index":index}, (err, results) => {
+       reply.send(results)
+    })
+  })
 
 // ********************************************
 // SERVERS ************************************
