64b88d31419debe3784335f77069a9f499a336fb
Aaron Strouse <aaronstrouse@gmail.com>
2019-02-03 16:50:37 -0500

    added put and post methods

--- Perl/dancer-server.pl
+++ Perl/dancer-server.pl
@@ -1,7 +1,5 @@
 use Dancer;
 use MongoDB;
-# Perl syntax: $ - single value; @ - list of values; % - hash or named array
-
 
 my $client = MongoDB->connect();
 my $quotes = $client->get_database('test')->get_collection('quotes');
@@ -20,13 +18,36 @@ get '/api/quotes' => sub {
         return \@results;
 };
 
+post '/api/quotes' => sub {
+        # Find the quote with the highest index and then add 1.
+    my $query = $quotes->find()->sort({'index' => -1})->limit(1);
+    my $topquote = $query->next;
+    my $max_id = $topquote->{'index'} + 1;
+    
+    # get the author and content from the parameters
+    if (!params->{content}) { #check for content
+        status 400;
+        return {message => "Content is required for new quotes."};
+    }
+
+    my %response = ( # sends a reference to a hash in the database for processing
+        'author' => params->{author},
+        'content' => params->{content},
+        'index' => $max_id
+    );
+
+    my $response = $quotes->insert_one(\%response);
+    status 201;
+    return {"index"=>$max_id};
+};
+
 get '/api/quotes/random' => sub {
-    my $response = $quotes->find()->sort({'index' => -1})->limit(1);
-    my $quote = $response->next;
-    my $max_number = $quote->{'index'};
-    my $random = int(rand($max_number));
-    my $answer = $quotes->find_one({"index" => $random});
-    return $answer;
+    my $max_item = $quotes->find()->sort({'index' => -1})->limit(1);
+    my $quote = $max_item->next;
+    my $max_id = $quote->{'index'};
+    my $random = int(rand($max_id));
+    my $response = $quotes->find_one({"index" => $random});
+    return $response;
 };
 
 get '/api/quotes/:index' => sub {
@@ -36,6 +57,25 @@ get '/api/quotes/:index' => sub {
         return;
     }
     return $response;
+};
+
+put '/api/quotes/:index' => sub {
+    my $index = int(params->{index});
+    if (!params->{content} && !params->{author}) {
+        status 400;
+        return {message => "Author or content are required for update"}
+    };
+    my $original = $quotes->find_one({index => $index});
+    my $content = params->{content} ? params->{content} : $original->{content};
+    my $author = params->{author} ? params->{author} : $original->{author};
+
+    my $response = $quotes->update_one({index => $index},
+    {
+        '$set' =>
+            {'author' => $author, 'content' => $content}
+    });
+    status 202;
+    return {"index"=>$index};
 };
 
 
